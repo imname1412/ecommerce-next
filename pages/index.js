622399054564e32bf44ad389c4d3e8bfc507eaec
merklePatt <pattarawut.mte@hotmail.com>
2022-07-16 11:11:03 +0700

    Create context for Cart

--- pages/index.js
+++ pages/index.js
@@ -1,59 +1,57 @@
-import React from 'react'
+import React from "react";
 import {
-  Cart ,
-  Footer ,
-  FooterBanner ,
-  HeroBanner ,
-  Layout ,
-  Navbar ,
-  Product ,
-} from '../components'
-import { client } from '../lib/client'
+  Cart,
+  Footer,
+  FooterBanner,
+  HeroBanner,
+  Layout,
+  Navbar,
+  Product,
+} from "../components";
+import { client } from "../lib/client";
 
-const Home = ({products , banner}) => {
+function ProductCom() {
+  return (
+    <div className="products-heading">
+      <h2>Best Selling Product</h2>
+      <p>Lorem ipsum dolor sit. Lorem, ipsum dolor.</p>
+    </div>
+  );
+}
 
-
+const Home = ({ products, banner }) => {
   return (
     <>
-    {console.log(products)}
-    {console.log(banner)}
+      {console.log(products)}
+      {console.log(banner)}
       <HeroBanner heroBanner={banner.length && banner[0]} />
 
-      <div className='products-heading'>
-        <h2>Best Selling Product</h2>
-        <p>Lorem ipsum dolor sit. Lorem, ipsum dolor.</p>
-      </div>
+      <ProductCom />
 
-
-      <div className='products-container'>
+      <div className="products-container">
         {products.map((product, index) => (
           <Product key={product._id} product={product} />
         ))}
       </div>
 
-
       <FooterBanner footerBanner={banner && banner[0]} />
-
     </>
-  )
-}
+  );
+};
 
 export const getServerSideProps = async () => {
-  const ProductQuery = '*[_type == "product"]'
-  const products = await client.fetch(ProductQuery)
+  const ProductQuery = '*[_type == "product"]';
+  const products = await client.fetch(ProductQuery);
 
-  const BannerQuery = '*[_type == "banner"]'
-  const banner = await client.fetch(BannerQuery)
-
+  const BannerQuery = '*[_type == "banner"]';
+  const banner = await client.fetch(BannerQuery);
 
   return {
     props: {
       products,
       banner,
-    }
-  }
-}
+    },
+  };
+};
 
-
-
-export default Home
+export default Home;
